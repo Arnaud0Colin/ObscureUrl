92c19c4d4fcd9976ebe9de43de1805dc0cdf1bab
Arnaud Colin <37111260+Arnaud0Colin@users.noreply.github.com>
2018-03-12 14:25:20 +0100

    Update AssemblyInfo.cs

--- ObscureUrl/ObscureUrl/Properties/AssemblyInfo.cs
+++ ObscureUrl/ObscureUrl/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("ObscureUrl")]
+[assembly: AssemblyTitle("SecurityExtention")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arnaud Colin")]
-[assembly: AssemblyProduct("ObscureUrl")]
+[assembly: AssemblyProduct("SecurityExtention")]
 [assembly: AssemblyCopyright("Copyright ©  2013-2018")]
 [assembly: AssemblyTrademark("Arnaud Colin")]
 [assembly: AssemblyCulture("")]
